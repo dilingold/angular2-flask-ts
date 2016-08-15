af60fd7381d4ecb349bb9c3362cc784b63b5b8bb
Joel Goldhirsch <Joel@Joels-MacBook-Pro.local>
2016-08-15 10:10:04 +1000

    initial commit

---
+++ static/js/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import {TabView} from 'primeng/primeng';
+import {TabPanel} from 'primeng/primeng';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My Second Angular 2 App</h1>'
+})
+export class AppComponent { }
